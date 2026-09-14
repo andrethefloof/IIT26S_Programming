516a52da9a1f0741345504e0eb5076d9af5f25b8
Vlad Andrei <enevla8900@gmail.com>
2026-09-14 20:31:16 +0300

    t4

--- Python_programming_W1_5.py
+++
@@ -1,8 +0,0 @@
-print("Calculate the area of a wall")
-Feed = input("Enter the width in meters: ")
-Width = int(Feed)       
-Feed = input("Enter the height in meters: ")
-Height = int(Feed)
-print("Width is", Width, "m and height is", Height, "m.")  
-Area = Width * Height
-print ("The wall will be", Area, "square meters.")
--- Python_programming_W1_6.py
+++
@@ -1,5 +0,0 @@
-Feed = input("Insert an integer: ")
-Value = int(Feed)
-remainder = Value % 2
-print("Value is:", Value)
-print("The remainder is", remainder, "when", Value, "is divided by 2.")

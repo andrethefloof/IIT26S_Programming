21615104e91dc6b2a138a9ff7acf928118d42b12
Vlad Andrei <enevla8900@gmail.com>
2026-09-14 10:45:45 +0300

    mrowmrow

--- A1_T2.py
+++ A1_T2.py
@@ -3,5 +3,5 @@ print(Name1)
 Name2 = "Harry"
 print(Name2)
 print(Name1 , Name2)
-print(Name1 , " is eating ice cream with " , Name2)
-print(Name1 , " and " , Name2 , " are friends")
+print(Name1,"is eating ice cream with",Name2)
+print(Name1,"and",Name2,"are friends")

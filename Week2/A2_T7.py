dd5c664516515e46b4dd318807e3abe3680a03f0
Vlad Andrei <enevla8900@gmail.com>
2026-09-21 11:16:16 +0300

    commt7

---
+++ Week2/A2_T7.py
@@ -0,0 +1,5 @@
+print("Program starting.\n")
+fahrenheit = float(input("Insert fahreneheits: "))
+celsius = (fahrenheit - 32) / 1.8
+print(f"{fahrenheit}°F is {round(celsius, 2)}°C")
+print("Program ending.")

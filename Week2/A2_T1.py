f26b5098f53852d8be08edec21402754ae314be2
Vlad Andrei <enevla8900@gmail.com>
2026-09-21 09:54:09 +0300

    t1update

--- Week2/A2_T1.py
+++ Week2/A2_T1.py
@@ -3,5 +3,6 @@ name = input("What is your name: ")
 float_num = float(input("Enter a floating point number: "))
 float_num_second = float(input("Enter second floating point number: "))
 print(name, "you gave numbers", float_num, "and", float_num_second)
-print("Multiplying first and second number will result in product", float_num * float_num_second)
+float_final = float_num * float_num_second
+print("Multiplying first and second number will result in product", round(float_final, 2))
 print("Program ending.")
